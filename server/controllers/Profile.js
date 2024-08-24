8a5b3bb6207a0b05fb90b4303e18060552284601
Sameer khan <121482820+Sameerkhan9412@users.noreply.github.com>
2024-08-24 19:32:03 +0530

    add profileDisplay and enrolled courses controller added

--- server/controllers/Profile.js
+++ server/controllers/Profile.js
@@ -1,46 +1,38 @@
 const Profile=require('../models/Profile')
-const User=require('../models/Users')
+const User=require('../models/Users');
+const { uploadImageToCloudinary } = require('../utils/imageUploader');
 
 // update profile
-exports.updateProfile=async(req,res)=>{
-    try {
-        // get data
-        const {dateOfBirth="",about="",contactNumber,gender}=req.body; 
-        // get user id
-        const userId=req.user.id;
-        // validation
-        if(!contactNumber||!gender||!id){
-            return res.status(400).json({
-                success:false,
-                message:"All fields are required, please try again"
-            })
-        }
-        // find profile
-        const userDetails=await User.findOne(userId);
-        const profileId=userDetails.additionalDetails;
-        const profileDetails=await Profile.findByIdAndUpdate(profileId)
+exports.updateProfile = async (req, res) => {
+	try {
+		const { dateOfBirth = "", about = "", contactNumber } = req.body;
+		const id = req.user.id;
 
-        // update profile
-        profileDetails.dateOfBirth=dateOfBirth;
-        profileDetails.about=about;
-        profileDetails.gender=gender;
-        profileDetails.contactNumber=contactNumber;
-        await profileDetails.save();    //here i used save bcz object bna hua h
+		// Find the profile by id
+		const userDetails = await User.findById(id);
+		const profile = await Profile.findById(userDetails.additionalDetails);
 
-        // return res
-        return res.status(200).json({
-            success:true,
-            message:"Profile updated successfully",
-            profileDetails
-        })
-    } catch (error) {
-        return res.status(500).json({
-            success:false,
-            message:"something went wrong while creating section",
-            error:error.message
-        })
-    }
-}
+		// Update the profile fields
+		profile.dateOfBirth = dateOfBirth;
+		profile.about = about;
+		profile.contactNumber = contactNumber;
+
+		// Save the updated profile
+		await profile.save();
+
+		return res.json({
+			success: true,
+			message: "Profile updated successfully",
+			profile,
+		});
+	} catch (error) {
+		console.log(error);
+		return res.status(500).json({
+			success: false,
+			error: error.message,
+		});
+	}
+};
 
 // delete account
 exports.deleteAccount=async(req,res)=>{
@@ -89,3 +81,58 @@ exports.getAllUserDetails=async(req,res)=>{
         })
     }
 }
+
+exports.updateDisplayPicture = async (req, res) => {
+    try {
+      const displayPicture = req.files.displayPicture
+      const userId = req.user.id
+      const image = await uploadImageToCloudinary(
+        displayPicture,
+        process.env.FOLDER_NAME,
+        1000,
+        1000
+      )
+      console.log(image)
+      const updatedProfile = await User.findByIdAndUpdate(
+        { _id: userId },
+        { image: image.secure_url },
+        { new: true }
+      )
+      res.send({
+        success: true,
+        message: `Image Updated successfully`,
+        data: updatedProfile,
+      })
+    } catch (error) {
+      return res.status(500).json({
+        success: false,
+        message: error.message,
+      })
+    }
+};
+  
+exports.getEnrolledCourses = async (req, res) => {
+    try {
+      const userId = req.user.id
+      const userDetails = await User.findOne({
+        _id: userId,
+      })
+        .populate("courses")
+        .exec()
+      if (!userDetails) {
+        return res.status(400).json({
+          success: false,
+          message: `Could not find user with id: ${userDetails}`,
+        })
+      }
+      return res.status(200).json({
+        success: true,
+        data: userDetails.courses,
+      })
+    } catch (error) {
+      return res.status(500).json({
+        success: false,
+        message: error.message,
+      })
+    }
+};
