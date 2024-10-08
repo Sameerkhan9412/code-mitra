1a2a1676b5f6d8df5e5746e31efb3f9053663aef
Sameer khan <121482820+Sameerkhan9412@users.noreply.github.com>
2024-10-08 18:06:33 +0530

    solve error

--- front-end/src/slices/ProfileSlice.js
+++ front-end/src/slices/ProfileSlice.js
@@ -1,22 +1,21 @@
-import {createSlice} from "@reduxjs/toolkit"
-
-const initialState = {
+import { createSlice } from "@reduxjs/toolkit";
+const initialState={
     user:localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")) : null,
     loading:false
-};
+}
 
-const profileSlice = createSlice({
-    name:"profile",
-    initialState: initialState,
-    reducers: {
-        setUser(state, value) {
-            state.user = value.payload;
-        },
-        setLoading(state, value) {
-            state.loading = value.payload;
-          },
+const profileSlice=createSlice({
+    name:"Profile",
+    initialState,
+    reducers:{
+        setUser(state,value){
+            state.user=value.payload
+        }
     },
-});
+    setLoading(state,value){
+        state.loading=value.payload;
+    }
+})
 
-export const {setUser,setLoading} = profileSlice.actions;
+export const {setUser,setLoading}=profileSlice.actions;
 export default profileSlice.reducer;
